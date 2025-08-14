fde16865ed435355ac278854376a327000dde18c
HaojieZhang123 <haojiezhang123@gmail.com>
2025-08-14 22:47:54 +0200

    first commit

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -0,0 +1,25 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/pizzas")
+public class PizzaController {
+
+    @Autowired
+    private PizzaRepository repository;
+
+    @GetMapping
+    public String index(Model model) {
+
+        model.addAttribute("pizzas", repository.findAll());
+
+        return "pizzas/index";
+    }
+
+}
